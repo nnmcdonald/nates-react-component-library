531a68eac23e741d8d6c621e89d5c06630bad371
Nate McDonald <mcdonaldnate7@gmail.com>
2021-10-18 23:42:25 -0700

    created basic LoadScreen

--- src/dev/App.js
+++ src/dev/App.js
@@ -1,9 +1,11 @@
+import { LoadScreen } from '../lib/components/UI/LoadScreen/LoadScreen';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       {/* Test components here */ }
+      <LoadScreen />
     </div>
   );
 }
