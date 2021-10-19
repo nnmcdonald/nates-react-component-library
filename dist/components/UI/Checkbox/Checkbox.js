c228ed14d5d2e07595917dc74a802903da58699d
Nate McDonald <mcdonaldnate7@gmail.com>
2021-10-18 21:53:09 -0700

    completed initial setup

---
+++ dist/components/UI/Checkbox/Checkbox.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Checkbox = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Checkbox = function Checkbox() {
+  return /*#__PURE__*/_react.default.createElement("div", null);
+};
+
+exports.Checkbox = Checkbox;
---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+export { LeadForm } from './components/LeadForm/LeadForm';
+export { Checkbox } from './components/UI/Checkbox/Checkbox';
+export { LoadScreen } from './components/UI/LoadScreen/LoadScreen';
