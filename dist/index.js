be9a155fed252beab7a9205b674d7df3696c770d
Nate McDonald <mcdonaldnate7@gmail.com>
2021-10-23 12:08:53 -0700

    trying to setup config for dev

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,37 @@
-export { LeadForm } from './components/LeadForm/LeadForm';
-export { Checkbox } from './components/UI/Checkbox/Checkbox';
-export { LoadScreen } from './components/UI/LoadScreen/LoadScreen';
-export { SearchBar } from './components/UI/SearchBar/SearchBar';
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Checkbox", {
+  enumerable: true,
+  get: function get() {
+    return _Checkbox.Checkbox;
+  }
+});
+Object.defineProperty(exports, "LeadForm", {
+  enumerable: true,
+  get: function get() {
+    return _LeadForm.LeadForm;
+  }
+});
+Object.defineProperty(exports, "LoadScreen", {
+  enumerable: true,
+  get: function get() {
+    return _LoadScreen.LoadScreen;
+  }
+});
+Object.defineProperty(exports, "SearchBar", {
+  enumerable: true,
+  get: function get() {
+    return _SearchBar.SearchBar;
+  }
+});
+
+var _LeadForm = require("./components/LeadForm/LeadForm");
+
+var _Checkbox = require("./components/UI/Checkbox/Checkbox");
+
+var _LoadScreen = require("./components/UI/LoadScreen/LoadScreen");
+
+var _SearchBar = require("./components/UI/SearchBar/SearchBar");
--- src/lib/index.js
+++
@@ -1,4 +0,0 @@
-export { LeadForm } from './components/LeadForm/LeadForm';
-export { Checkbox } from './components/UI/Checkbox/Checkbox';
-export { LoadScreen } from './components/UI/LoadScreen/LoadScreen';
-export { SearchBar } from './components/UI/SearchBar/SearchBar';
