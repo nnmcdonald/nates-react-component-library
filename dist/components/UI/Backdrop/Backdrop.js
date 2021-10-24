0142064ffb28a961bd7624372fe16d6623716d3a
Nate McDonald <mcdonaldnate7@gmail.com>
2021-10-23 22:35:29 -0700

    forgot to build

--- dist/components/UI/Backdrop/Backdrop.js
+++ dist/components/UI/Backdrop/Backdrop.js
@@ -14,13 +14,15 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Backdrop = function Backdrop(props) {
+  var darkTheme = props.darkTheme ? _BackdropModule.default.darkTheme : '';
   return /*#__PURE__*/_react.default.createElement("div", {
     onClick: props.onClick,
-    className: _BackdropModule.default.backdrop
+    className: "".concat(_BackdropModule.default.backdrop, " ").concat(darkTheme)
   }, props.children);
 };
 
 exports.Backdrop = Backdrop;
 Backdrop.propTypes = {
-  onClick: _propTypes.default.func
+  onClick: _propTypes.default.func,
+  darkTheme: _propTypes.default.bool
 };
